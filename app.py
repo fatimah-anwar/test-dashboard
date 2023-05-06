705b0e02229e75539c34fdb63d70090b3469854b
Fatimah Anwar <fatimah235@Fatimahs-MBP.modem>
2023-05-07 00:13:00 +1000

    First commit

---
+++ app.py
@@ -0,0 +1,151 @@
+import pandas as pd
+from dash import Dash, Input, Output, dcc, html
+
+data = (
+    pd.read_csv("avocado.csv")
+    .assign(Date=lambda data: pd.to_datetime(data["Date"], format="%Y-%m-%d"))
+    .sort_values(by="Date")
+)
+regions = data["region"].sort_values().unique()
+avocado_types = data["type"].sort_values().unique()
+
+external_stylesheets = [
+    {
+        "href": (
+            "https://fonts.googleapis.com/css2?"
+            "family=Lato:wght@400;700&display=swap"
+        ),
+        "rel": "stylesheet",
+    },
+]
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+app.title = "Dashboard"
+
+app.layout = html.Div(
+    children=[
+        html.Div(
+            children=[
+                html.H1(
+                    children="Cluster and Cloud Computing Assignment 2", className="header-title"
+                ),
+                html.H3(
+                    children=("Australia Social Media Analytics on the Cloud"),
+                    className="header-description",
+                ),
+            ],
+            className="header",
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=[
+                        html.Div(children="Scenario", className="menu-title"),
+                        dcc.Dropdown(
+                            id="scenario-filter",
+                            options=[
+                                {"label": region, "value": region}
+                                for region in regions
+                            ],
+                            value="Albany",
+                            clearable=False,
+                            searchable=False,
+                            className="dropdown",
+                        ),
+                    ]
+                ),
+                html.Div(
+                    children=[
+                        html.Div(children="Attribute", className="menu-title"),
+                        dcc.Dropdown(
+                            id="attribute-filter",
+                            options=[
+                                {
+                                    "label": avocado_type.title(),
+                                    "value": avocado_type,
+                                }
+                                for avocado_type in avocado_types
+                            ],
+                            value="organic",
+                            clearable=False,
+                            searchable=False,
+                            className="dropdown",
+                        ),
+                    ],
+                ),
+            ],
+            className="menu",
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(
+                        id="price-chart",
+                        config={"displayModeBar": False},
+                    ),
+                    className="card",
+                ),
+                html.Div(
+                    children=dcc.Graph(
+                        id="volume-chart",
+                        config={"displayModeBar": False},
+                    ),
+                    className="card",
+                ),
+            ],
+            className="wrapper",
+        ),
+    ]
+)
+
+
+@app.callback(
+    Output("price-chart", "figure"),
+    Output("volume-chart", "figure"),
+    Input("scenario-filter", "value"),
+    Input("attribute-filter", "value"),
+)
+def update_charts(scenario, attribute):
+    filtered_data = data.query(
+        "region == @scenario and type == @attribute"
+    )
+    price_chart_figure = {
+        "data": [
+            {
+                "x": filtered_data["Date"],
+                "y": filtered_data["AveragePrice"],
+                "type": "lines",
+                "hovertemplate": "$%{y:.2f}<extra></extra>",
+            },
+        ],
+        "layout": {
+            "title": {
+                "text": "Figure 1",
+                "x": 0.05,
+                "xanchor": "left",
+            },
+            "xaxis": {"fixedrange": True},
+            "yaxis": {"tickprefix": "$", "fixedrange": True},
+            "colorway": ["#17B897"],
+        },
+    }
+
+    volume_chart_figure = {
+        "data": [
+            {
+                "x": filtered_data["Date"],
+                "y": filtered_data["Total Volume"],
+                "type": "lines",
+            },
+        ],
+        "layout": {
+            "title": {"text": "Figure 2", "x": 0.05, "xanchor": "left"},
+            "xaxis": {"fixedrange": True},
+            "yaxis": {"fixedrange": True},
+            "colorway": ["#E12D39"],
+        },
+    }
+    return price_chart_figure, volume_chart_figure
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
